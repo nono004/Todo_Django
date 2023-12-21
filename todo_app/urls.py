4adb4ee83577037706e0ffb1e7b7cb1a5499a130
nono <nonoka.abe.24@gmail.com>
2023-12-21 22:46:05 +0900

    version 1

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import Main, Input, Complete
+from .views import Main, Input, Complete, DeleteItem
 
-app_name = 'todo_app'
+app_name = 'todoApp'
 
 urlpatterns = [
     path("", Main.as_view(), name='Top'),
+    path('delete/<int:item_id>/', DeleteItem.as_view(), name='Delete'),
     path('input/', Input.as_view(), name='Input'),
     path('complete/', Complete.as_view(), name='Complete')
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,26 +1,34 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from .forms import InputForm
+from .models import TodoModel
 
 # Create your views here.
 class Main(View):
     def get(self, request):
-        return render(request, "todo_app/main.html")
+        data = TodoModel.objects.all()
+        return render(request, "todoApp/main.html", {'data' : data})
+
+class DeleteItem(View):
+    def post(self, request, item_id):
+        item = TodoModel.objects.get(id=item_id)
+        item.delete()
+        return redirect('/todoApp/')
 
 class Input(View):
     def get(self, request):
         form = InputForm()
-        return render(request, "todo_app/input.html", {'form':form})
+        return render(request, "todoApp/input.html", {'form':form})
 
     def post(self, request):
         form = InputForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('/todo_app/complete/')
+            return redirect('/todoApp/complete/')
         else:
             form = InputForm()
-        return render(request, 'todo_app/input.html', {'form':form})
+        return render(request, 'todoApp/input.html', {'form':form})
 
 class Complete(View):
     def get(self, request):
-        return render(request, "todo_app/complete.html")
+        return render(request, "todoApp/complete.html")
