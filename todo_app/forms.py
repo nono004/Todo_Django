9590c7c8fab1d495f353bdcb83c177c69fa7bc63
nono <nonoka.abe.24@gmail.com>
2024-01-05 14:56:35 +0900

    commit

--- todo_app/forms.py
+++ todo_app/forms.py
@@ -7,3 +7,13 @@ class InputForm(forms.ModelForm):
     class Meta:
         model = TodoModel
         fields = ['Todo', 'DeadLine', 'Category']
+        widgets = {
+            'Todo': forms.Textarea(attrs={'class': 'textarea', 'placeholder': 'ToDoを入力'}),
+            'DeadLine': forms.DateInput(attrs={'type': 'date', 'class': 'date'}),
+            'Category' : forms.Select(attrs={'class': 'Category'})
+        }
+        labels = {
+            'Todo': 'ToDo',  # ToDoフィールドのラベルを変更
+            'DeadLine': '期限',  # DeadLineフィールドのラベルを変更
+            'Category': 'カテゴリ',  # Categoryフィールドのラベルを変更
+        }
